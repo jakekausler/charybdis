33f619a8e7170a5a7caa26b42b43b8c3cd568cd6
jakekausler <jakekausler@yahoo.com>
2023-08-12 16:26:51 +0000

    More work on the shield

--- bin/build.sh
+++ bin/build.sh
@@ -8,11 +8,11 @@ TIMESTAMP="${TIMESTAMP:-$(date -u +"%Y%m%d%H%M%S")}"
 west update
 
 # West Build (left)
-west build -s zmk/app -d build/left -b nice_nano_v2 -- -DSHIELD=charybdis_left -DZMK_CONFIG="${PWD}/config"
+west build -s zmk/app -d build/left -b nice_nano_v2 -- -DSHIELD=charybdis1_left -DZMK_CONFIG="${PWD}/config"
 # Adv360 Left Kconfig file
 cat build/left/zephyr/.config | grep -v "^#" | grep -v "^$"
 # West Build (right)
-west build -s zmk/app -d build/right -b -- -DSHIELD=charybdis_right -DZMK_CONFIG="${PWD}/config"
+west build -s zmk/app -d build/right -b nice_nano_v2 -- -DSHIELD=charybdis1_right -DZMK_CONFIG="${PWD}/config"
 # Adv360 Right Kconfig file
 cat build/right/zephyr/.config | grep -v "^#" | grep -v "^$"
 # Rename zmk.uf2
